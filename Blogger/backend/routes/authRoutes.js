ee728e936bc4a3aca4c5afb19afcaa05df6c0ece
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 21:00:25 +0530

    Update authRoutes.js

--- Blogger/backend/routes/authRoutes.js
+++ Blogger/backend/routes/authRoutes.js
@@ -8,8 +8,6 @@ router.post('/register', authController.registerUser);
 // Route for user login
 router.post('/login', authController.loginUser);
 
-// Route for user logout
-router.post('/logout', authController.logoutUser);
 
 
 module.exports = router;
