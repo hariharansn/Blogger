cc705111adfdea2838837b39de329669262e0b6d
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 21:00:43 +0530

    Update commentRoutes.js

--- Blogger/backend/routes/commentRoutes.js
+++ Blogger/backend/routes/commentRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const commentController = require('../controllers/commentController');
-const authMiddleware = require('../middleware/authentication');
+const { authenticateToken } = require('../middleware/authentication');
 
 // Route for creating a new comment
-router.post('/', commentController.createComment);
+router.post('/',authenticateToken, commentController.createComment);
 
 // Route for getting all comments of a post
-router.get('/posts/:postId/comments', commentController.getCommentsByPostId);
+router.get('/', commentController.getCommentsByPostId);
 
 // Route for getting a specific comment
-router.get('/comments/:commentId', commentController.getCommentById);
+router.get('/:commentId', commentController.getCommentById);
 
 // Route for updating a comment
-router.put('/:commentId', commentController.updateComment);
+router.put('/:commentId',authenticateToken, commentController.updateComment);
 
 // Route for deleting a comment
-router.delete('/:commentId',  commentController.deleteComment);
+router.delete('/:commentId',  authenticateToken,commentController.deleteComment);
 
 module.exports = router;
