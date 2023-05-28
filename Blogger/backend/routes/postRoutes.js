660da8823efea1db22178f2e1409212ace6e86f3
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 21:02:50 +0530

    Update postRoutes.js

--- Blogger/backend/routes/postRoutes.js
+++ Blogger/backend/routes/postRoutes.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
-const { authenticate } = require('../middleware/authentication');
+const { authenticateToken } = require('../middleware/authentication');
 
-router.post('/posts', authenticate, postController.createPost);
-router.get('/posts', postController.getAllPosts);
-router.get('/posts/:postId', postController.getPostById);
-router.put('/posts/:postId', authenticate, postController.updatePost);
-router.delete('/posts/:postId', authenticate, postController.deletePost);
+
+// Route for creating a new post
+router.post('/', authenticateToken, postController.createPost);
+
+// Route for getting all post
+router.get('/', postController.getAllPosts);
+
+// Route for getting a specific post
+router.get('/:postId', postController.getPostById);
+
+
+// Route for updating a post
+router.put('/:postId', authenticateToken, postController.updatePost);
+
+
+// Route for deleting a post
+router.delete('/:postId', authenticateToken, postController.deletePost);
 
 module.exports = router;
