ba83b4fd975ef08dfe3b5303b284548f5f0a76c5
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 21:13:04 +0530

    Update database.js

--- Blogger/backend/config/database.js
+++ Blogger/backend/config/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 const initializeTables = require('../models/model');
+require('dotenv').config();
 
 // Create Sequelize instance
-const sequelize = new Sequelize('production', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5432,
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
   timezone: '+05:30',
   define: {
     // Global options for model definition
