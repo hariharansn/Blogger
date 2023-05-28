ca275f063ea231f69473c4fa60cee73246076b15
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 20:56:36 +0530

    Update postController.js

--- Blogger/backend/controllers/postController.js
+++ Blogger/backend/controllers/postController.js
@@ -1,102 +1,134 @@
-const Post = require('../models/post');
 
-const createPost = async (req, res) => {
+const {User,Post,Comment}  =require('../models/model')
+
+
+// Create a new blog post
+exports.createPost = async (req, res, next) => {
+  const { title, content } = req.body;
+
   try {
-    const { title, content } = req.body;
+    // Get the user ID from the request
+    const userId = req.userId;
 
-    // Check if the user ID is available in the req.user object
-    if (!req.user || !req.user.id) {
-      return res.status(401).json({ message: 'User authentication failed' });
-    }
+    // Create the post in the database
+    const post = await Post.create({ title, content, userId });
+    
+    res.status(201).json(post);
+  } catch (err) {
+    console.error('Error creating post:', err);
+    next(err);
+  }
+};
 
-    const userId = req.user.id;
 
-    // Create a new post
-    const newPost = await Post.create({
-      title,
-      content,
-      userId,
+
+
+// Get all blog posts
+exports.getAllPosts =  async (req, res, next) => {
+  try {
+    // Get all posts from the database
+    const posts = await Post.findAll({ include: [{ model: User, attributes: ['username'] }] });
+
+    res.status(200).json({ posts });
+  } catch (err) {
+    console.error('Error retrieving posts:', err);
+    next(err);
+  }
+};
+
+
+
+// Get a specific post
+exports.getPostById =  async (req, res, next) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await Post.findByPk(postId, {
+      include: { model: Comment, order: [['created_at', 'ASC'], ['updated_at', 'ASC']] }, // Include the associated Comment model and order by created_at ASC
     });
 
-    res.status(201).json({ message: 'Post created', post: newPost });
-  } catch (error) {
-    console.error('Post creation error:', error);
-    res.status(500).json({ message: 'Failed to create post' });
-  }
-};
-
-const getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.findAll();
-    res.status(200).json({ posts });
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    res.status(500).json({ message: 'Failed to fetch posts' });
-  }
-};
-
-const getPostById = async (req, res) => {
-  try {
-    const postId = req.params.postId;
-
-    const post = await Post.findByPk(postId);
-
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ error: 'Post not found' });
     }
 
-    res.status(200).json({ post });
-  } catch (error) {
-    console.error('Error fetching post:', error);
-    res.status(500).json({ message: 'Failed to fetch post' });
+    res.status(200).json(post);
+  } catch (err) {
+    console.error('Error retrieving post:', err);
+    next(err);
   }
 };
 
-const updatePost = async (req, res) => {
+
+
+
+
+// Update a post
+exports.updatePost =async (req, res, next) => {
   try {
-    const postId = req.params.postId;
+    const { postId } = req.params;
     const { title, content } = req.body;
+    const userId = req.userId; // Assuming req.userId contains the user ID
 
     const post = await Post.findByPk(postId);
 
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    if (post.userId !== userId) {
+      return res.status(403).json({ error: 'Access denied' });
     }
 
     post.title = title;
     post.content = content;
     await post.save();
 
-    res.status(200).json({ message: 'Post updated', post });
-  } catch (error) {
-    console.error('Post update error:', error);
-    res.status(500).json({ message: 'Failed to update post' });
+    res.status(200).json({ message: 'Post updated successfully', post });
+  } catch (err) {
+    console.error('Error updating post:', err);
+    next(err);
   }
 };
 
-const deletePost = async (req, res) => {
+
+
+// Delete a post
+exports.deletePost =async (req, res, next) => {
   try {
-    const postId = req.params.postId;
+    const { postId } = req.params;
+    const { userId } = req;
 
     const post = await Post.findByPk(postId);
 
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ error: 'Post not found' });
     }
 
+    if (post.userId !== userId) {
+      return res.status(403).json({ error: 'Access denied' });
+    }
+
+    // Delete the post
     await post.destroy();
 
-    res.status(200).json({ message: 'Post deleted' });
-  } catch (error) {
-    console.error('Post deletion error:', error);
-    res.status(500).json({ message: 'Failed to delete post' });
+    // Delete the associated comments
+    await Comment.destroy({ where: { postId } });
+
+    res.status(200).json({ message: 'Post and associated comments deleted successfully' });
+  } catch (err) {
+    console.error('Error deleting post:', err);
+    next(err);
   }
 };
 
-module.exports = {
-  createPost,
-  getAllPosts,
-  getPostById,
-  updatePost,
-  deletePost,
-};
+
+
+
+
+
+
+
+
+
+
+
