cf601fb52a62fa766bcb95279e832050a18a301f
hariharansn <93645614+hariharansn@users.noreply.github.com>
2023-05-28 20:27:04 +0530

    Update database.js

--- Blogger/backend/config/database.js
+++ Blogger/backend/config/database.js
@@ -1,10 +1,21 @@
 const { Sequelize } = require('sequelize');
+const initializeTables = require('../models/model');
 
 // Create Sequelize instance
-const sequelize = new Sequelize('test', 'postgres', 'password', {
+const sequelize = new Sequelize('production', 'postgres', 'password', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432,
+  timezone: '+05:30',
+  define: {
+    // Global options for model definition
+    timestamps: true,
+    underscored: true
+  },
+  dialectOptions: {
+    useUTC: false,
+    useIST:true,
+  },
 });
 
 // Test the database connection
@@ -12,6 +23,7 @@ sequelize
   .authenticate()
   .then(() => {
     console.log('Database connected');
+    initializeTables();
   })
   .catch((err) => {
     console.error('Database connection failed:', err);
